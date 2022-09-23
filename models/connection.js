310595c4c70dde9f2aed5f8041525c8e56ff3846
DHANANJAY123-HUB <75534106+DHANANJAY123-HUB@users.noreply.github.com>
2022-09-23 16:16:28 +0530

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
-const url = "mongodb://localhost:27017/Tamu_Project"
+//const url = "mongodb://localhost:27017/Tamu_Project"
+mongoose.connect('mongodb+srv://user:user@cluster0.lcji4ad.mongodb.net/Demo?retryWrites=true&w=majority',{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+})
 mongoose.connect(url)
 const db = mongoose.connection
 console.log("Successfully connected to mongodb database")
